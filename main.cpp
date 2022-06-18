23394c3121cfc59b89f40487839607acc8660d65
mj426382 <102721390+JakubBraun@users.noreply.github.com>
2022-06-18 11:14:01 +0200

    Add basic template with cli support

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        cout << "musisz podać 2 nazwy plików\n";
+        return 0;
+    }
+    return 0;
+}
