77321d8920f5918165177c94fb1fb600a753c9e9
mj426382 <102721390+JakubBraun@users.noreply.github.com>
2022-06-18 11:18:24 +0200

    Add files opener

--- main.cpp
+++ main.cpp
@@ -8,5 +8,15 @@ int main(int argc, char *argv[])
         cout << "musisz podać 2 nazwy plików\n";
         return 0;
     }
+    string first_file = argv[1];
+    string second_file = argv[2];
+
+    ifstream first_stream(first_file);
+    ifstream second_stream(second_file);
+
+
+    first_stream.close();
+    second_stream.close();
+
     return 0;
 }
